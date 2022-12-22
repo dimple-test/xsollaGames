167c1c843ce1452818b5fbbf865de96bf85b9fd3
dimpal <dimpalrana52@gmail.com>
2022-12-22 15:14:13 -0500

    config changes

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,20 @@
-FROM php:7.0-fpm
+FROM php:7.4-fpm
 #Install git
 RUN apt-get update \
     && apt-get install -y git
 RUN docker-php-ext-install pdo pdo_mysql mysqli
 #RUN a2enmod rewrite
+
 #Install Composer
 RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 RUN php composer-setup.php --install-dir=. --filename=composer
 RUN mv composer /usr/local/bin/
-COPY app /var/www/html/
+
+WORKDIR /var/www/html
+
+# install application dependencies
+# COPY composer.json ./
+# RUN composer install
+
+COPY . /var/www/html/
+#CMD [ "php", "-S", "0.0.0.0:8080", "-t", "public" ]
--- app/index.php
+++ app/index.php
@@ -1,74 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    </head>
-    <body>
-        <main>
-            <div class="container">
-                <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <img src="assets/img/logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                </a>
-                <ul class="nav nav-pills">
-                    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-                </ul>
-                </header>
-                <div>
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Platform</th>
-                            <th scope="col">Star Rating</th>
-                            <th scope="col">Review Text</th>
-                            <th scope="col">Last_played</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            </tr>
-                            <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            </tr>
-                            <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                    <p class="col-md-4 mb-0 text-muted">&copy; 2022 Company, Inc</p>
-                    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                        <img src="assets/img/logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                    </a>
-                    <ul class="nav col-md-4 justify-content-end">
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-                    </ul>
-                </footer>
-            </div>
-        </main>
-        <script src="assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    </body>
+<?php
+    require_once('config/init.php');
 
-</html>
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    $app = new App();
+?>
